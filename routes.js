854fa74018a86217aaad8e779513b12a3353b606
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-06-29 23:14:35 +0300

    links are cleaned

--- routes.js
+++ routes.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Import our Page Routes
-const pageRoutes = require('./routes/pages');
-const blogsRoutes = require('./routes/blogs');
-const authorsRoutes = require('./routes/authors');
-const sessionsRoutes = require('./routes/sessions');
+const blogsRoutes = require("./routes/blogs");
+const authorsRoutes = require("./routes/authors");
+const sessionsRoutes = require("./routes/sessions");
 
 // Register our Page Routes with our app
-app.use('/', pageRoutes);
-app.use('/blogs', blogsRoutes);
-app.use('/authors', authorsRoutes);
-app.use('/', sessionsRoutes);
+app.use("/blogs", blogsRoutes);
+app.use("/authors", authorsRoutes);
+app.use("/", sessionsRoutes);
 
 // Export our changes
 module.exports = app;
--- routes/pages.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-// Our controllers
-const PagesController = require("../controllers/pagesController");
-
-// Create our routes
-router.get(`/`, PagesController.show);
-
-module.exports = router;
