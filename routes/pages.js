2352ddbed47e18ff4bf478d66122b5cd018808bd
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-06-29 22:54:54 +0300

    cleaning pages

--- routes/pages.js
+++ routes/pages.js
@@ -1,11 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Our controllers
-const PagesController = require('../controllers/pagesController');
+const PagesController = require("../controllers/pagesController");
 
 // Create our routes
 router.get(`/`, PagesController.show);
-router.get(`/about`, PagesController.show);
-router.get(`/contact`, PagesController.show);
 
 module.exports = router;
