59a705ddf0e85814f3bd53d8e21dc1bf3deb9144
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-07-05 22:01:19 +0300

    removing unneccessary models to avoid confusion

--- routes.js
+++ routes.js
@@ -2,13 +2,11 @@ const express = require("express");
 const app = express();
 
 // Import our Page Routes
-const friendsRoutes = require("./routes/friends");
 const authorsRoutes = require("./routes/authors");
 const sessionsRoutes = require("./routes/sessions");
 const chatsRoutes = require("./routes/chats");
 
 // Register our Page Routes with our app
-app.use("/friends", friendsRoutes);
 app.use("/chats", chatsRoutes);
 app.use("/authors", authorsRoutes);
 app.use("/", sessionsRoutes);
