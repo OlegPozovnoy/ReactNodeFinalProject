82486c4de5631a05b7599a2c389c91b5c91b5132
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2019-08-11 09:47:34 +0300

    !

---
+++ client/build/precache-manifest.a177ad48305634032abf98ca820ee54c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671623244f9aaa30a9c66a9a38b1c35d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2faa1de77388a7152b30",
+    "url": "/static/js/2.1b96eeca.chunk.js"
+  },
+  {
+    "revision": "d47062453ed064691bec",
+    "url": "/static/js/main.ccf7daf9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
